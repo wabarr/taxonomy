6dad2e1e8a8c94cc434be0f8805c5904afc2919c
wabarr <wabarr@gmail.com>
2015-07-16 17:19:28 -0400

    add taxonomy app

--- taxonomy/models.py
+++ taxonomy/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Rank(models.Model):
+    sortOrder = models.IntegerField()
+    name = models.CharField(max_length=30)
+    parent = models.ForeignKey('self', null=True, blank=True)
+
+class Taxon(models.Model):
+    rank = models.ForeignKey(Rank)
+    name = models.CharField(max_length=30)
+    parent = models.ForeignKey('self', null=True, blank=True)
