9ab7074f133b150f40b4d1ef55d4a9fb526db4e1
wabarr <wabarr@gmail.com>
2015-07-17 14:16:51 -0400

    make inline proper class

--- references/admin.py
+++ references/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from references.models import *
 from ajax_select import make_ajax_form
-from ajax_select.admin import AjaxSelectAdmin
+from ajax_select.admin import AjaxSelectAdmin, AjaxSelectAdminTabularInline
 
 
-class AuthorOrderInline(admin.TabularInline):
+class AuthorOrderInline(AjaxSelectAdminTabularInline):
     model = AuthorOrder
     extra = 2
     form = make_ajax_form(AuthorOrder, {"author":"authorLookup"})
+
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['id', '__unicode__']
