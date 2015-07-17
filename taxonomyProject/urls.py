eac764d962f1bbe0fd9684d383275160bcb580f9
wabarr <wabarr@gmail.com>
2015-07-17 14:44:53 -0400

    add api filtering

--- taxonomyProject/urls.py
+++ taxonomyProject/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from taxonomy.models import Taxon, Rank
 from references.models import Reference
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets, generics
 from ajax_select import urls as ajax_select_urls
 
 # Serializers define the API representation.
@@ -10,18 +10,25 @@ class TaxonSerializer(serializers.HyperlinkedModelSerializer):
     rank = serializers.PrimaryKeyRelatedField(queryset=Rank.objects.all())
     ref = serializers.PrimaryKeyRelatedField(queryset=Reference.objects.all(), required=False)
     parent = serializers.PrimaryKeyRelatedField(queryset=Taxon.objects.all(), required=False)
+
     class Meta:
         model = Taxon
         fields = ('url', 'name', 'rank', 'ref', 'parent')
 
 # ViewSets define the view behavior.
-class TaxonViewSet(viewsets.ModelViewSet):
-    queryset = Taxon.objects.all()
+class TaxonList(viewsets.ModelViewSet):
     serializer_class = TaxonSerializer
+
+    def get_queryset(self):
+        queryset = Taxon.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'taxa', TaxonViewSet)
+router.register(r'taxa', TaxonList, base_name="taxon")
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
