8a694bea50cd03d1d03005e35d79baff569ef6cc
wabarr <wabarr@gmail.com>
2015-07-20 14:48:05 -0400

    add modest validation for ranks

--- taxonomy/models.py
+++ taxonomy/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from references.models import Reference
-
+from django.core.validators import ValidationError
 
 class Rank(models.Model):
     sortOrder = models.IntegerField()
@@ -25,16 +25,30 @@ class Taxon(models.Model):
     def fullTaxonomy(self):
         #returns ordered list of all parent taxa
         if self.parent is None:
-            return None
+            return [self.__str__()]
 
         else:
             parents = []
             parents.append(self)
             current = self.parent
             while current is not None:
-                parents.append(current.parent)
+                parents.append(current)
                 current = current.parent
-            return [element for element in reversed(parents) if element is not None]
+
+            return [element.__str__() for element in reversed(parents) if element is not None]
+
+    def clean(self, *args, **kwargs):
+        if self.parent:
+            if not self.parent.rank.sortOrder < self.rank.sortOrder:
+                raise ValidationError("Parent taxon much be from a more inclusive taxon in the hierarchy")
+        if self.parent is None and self.rank.name <> 'kingdom':
+            raise ValidationError("A taxon with no parent must be at the rank of 'kingdom'.")
+
+        super(Taxon, self).clean(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(Taxon, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "taxa"
