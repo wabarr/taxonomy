b41f1413f083c945ce620c475fb0497d964a441e
wabarr <wabarr@gmail.com>
2015-07-20 15:36:04 -0400

    add ajax select for parent lookup

--- taxonomy/admin.py
+++ taxonomy/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from taxonomy.models import *
+from ajax_select import make_ajax_form
+from ajax_select.admin import AjaxSelectAdmin
 
 class RankAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "sortOrder", "parent"]
     list_editable = ["name", "sortOrder", "parent"]
 
-class TaxonAdmin(admin.ModelAdmin):
+class TaxonAdmin(AjaxSelectAdmin):
     list_display = ["name", "rank", "parent", "ref"]
     list_filter = ["rank"]
-
+    form = make_ajax_form(Taxon, {"parent":"taxonLookup"})
 
 
 admin.site.register(Taxon, TaxonAdmin)
