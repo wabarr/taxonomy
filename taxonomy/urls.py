66b1e845247727ed3f93591e23d533403a730ddc
wabarr <wabarr@gmail.com>
2015-07-30 11:36:36 -0400

    fix url scheme

--- taxonomy/urls.py
+++ taxonomy/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^add/$', AddTaxa.as_view(), name="add-taxon"),
     url(r'^update/(?P<pk>\d+)/$', ChangeTaxon.as_view(), name="change-taxon"),
     url(r'^bulk_update/', bulk_update, name="bulk-update"),
-    url(r'', TaxonList.as_view(), name="taxon-list")
+    url(r'^$', TaxonList.as_view(), name="taxon-list")
 ]
